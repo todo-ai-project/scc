550a9b333cbb7542f46a5e913d87512fe083a655
o9ioakd2 <comin0910@gmail.com>
2026-05-11 15:40:59 +0900

    Fix: Remove embedded git repositories and combine into one

---
+++ backend/src/routes/todo.js
@@ -0,0 +1,12 @@
+//backend>src>routes>todo.js
+const express = require('express');
+const router = express.Router();
+
+const todoController = require('../controllers/todoController');
+const aiController = require('../controllers/aiController'); 
+
+router.post('/', todoController.createTodo);
+router.post('/generate', aiController.generateTodoMate);
+router.delete('/:id', todoController.deleteTodo);
+
+module.exports = router;
